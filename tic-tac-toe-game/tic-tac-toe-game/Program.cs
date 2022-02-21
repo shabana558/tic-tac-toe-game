85017ae0a96b9337b888a8bad296170925ec0135
shaik ameer basha <shaikshabana758@gmail.com>
2022-02-21 10:22:25 +0530

    [shabana] add tic-tac-toe-game

---
+++ tic-tac-toe-game/tic-tac-toe-game/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tic_tac_toe_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome");
+            tic_tac_toe_game.Initializing();
+            Console.ReadLine();
+         
+        }
+
+      
+    }
+}
+
+          
---
+++ tic-tac-toe-game/tic-tac-toe-game/tic-tac-toe-game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tic_tac_toe_game
+{
+    internal class tic_tac_toe_game
+    {
+        public static char[] Initializing()
+        {
+            char[] board = new char[10];
+            for (int i = 0; i < board.Length; i++)
+            {
+                board[i] = ' ';
+
+            }
+            return board;
+        }
+    }
+}
+
+
